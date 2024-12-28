61439651a742d9a798c70f36629a8db5abc5669c
Exo <66042843+NotExpo@users.noreply.github.com>
2024-12-28 00:25:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function message() {
     let egg = document.getElementById("egg").value;
 
     const webhook = "https://discord.com/api/webhooks/1322415895699783761/43Ecjna1dKYVsTHEP7a38MhWB9ZSQV4Gx6j24TT7xOY-CV6wxcpYH9Ir_e5Fes4VMNbT";
-    const contents = `Name: ${name}\nFlavor: ${flavor}\nEgg: ${egg}\n-------`;
+    const contents = `Name: ${name}\nFlavor: ${flavor}\nEgg: ${egg}\n-----------------------`;
     const request = new XMLHttpRequest();
     request.open("POST", webhook);
     request.setRequestHeader('Content-type', 'application/json');
